9cb31c1bfb4fe767175b9514a226c2c88cfb0b4e
Raúl Olán <raulolan@hotmail.com>
2019-03-17 12:21:39 -0600

    Reto 6

---
+++ reto6/Diagrama.java
@@ -0,0 +1,16 @@
+package reto6;
+
+public class Diagrama {
+    public int cantidad;
+    public String nombres;
+    private boolean esValido;
+    
+    void PrimerMetodoArregloU(int[] a){   
+        
+    }
+    
+    void SegundoMetodoArregloM(int[] b){   
+        
+    }
+}
+
